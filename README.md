20909af209c43f097b1cff00bd941e9798947824
Stefan Arentz <stefan@arentz.ca>
2014-08-31 14:34:02 -0400

    Updates

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a plugin for the Dovecot IMAP server. It does nothing, it is
+just to figure out how to write a very minimal plugin; how to compile,
+install and make Dovecot load it.
+
+(So far, no success, I think the plugin is correct, but Dovecot does
+not load it. At least i don't think so, since I do not see any of the
+debug messages that it is supposed to print.)
--- empty-plugin.c
+++ empty-plugin.c
@@ -1,23 +1,12 @@
 
 #include "lib.h"
 #include "module-dir.h"
-#include "module-context.h"
-#include "imap-common.h"
-#include "imap-commands.h"
-#include "mail-storage-private.h"
 
 const char *empty_plugin_version = DOVECOT_VERSION;
 
-struct empty_client {
-  union imap_module_context module_ctx;
-  int something;
-};
-
 static struct module *empty_module;
 
-static MODULE_CONTEXT_DEFINE_INIT(empty_imap_module, &imap_module_register);
-
-void empty_plugin_init(struct module *module ATTR_UNUSED)
+void empty_plugin_init(struct module *module)
 {
     i_debug("empty_plugin_init");
     empty_module = module;
